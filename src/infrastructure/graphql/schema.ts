46a123fd033de47444bcd38d732812ce8df8e027
sebas3261 <sebassanchez3261@gmail.com>
2025-10-31 22:03:08 -0500

    graphql types

--- src/infrastructure/graphql/schema.ts
+++ src/infrastructure/graphql/schema.ts
@@ -1,35 +1,24 @@
 import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-  type CatBreed {
-    id: ID!
-    name: String!
-    origin: String
-    temperament: String
-    imageUrl: String
-  }
-
   type Student {
     id: ID!
-    name: String!
+    name: String
     email: String
     age: Int
   }
 
-  type CatImage {
+  type Cat {
     id: ID!
     url: String!
     width: Int
     height: Int
-    breedId: String
   }
 
   type Query {
-    catBreed(id: ID!): CatBreed
-    students: [Student!]!
-
-    # Nuevas:
-    randomCatImages(limit: Int = 1): [CatImage!]!
-    breedCatImages(breedId: ID!, limit: Int = 1): [CatImage!]!
+    students: [Student]
+    randomCatImages(limit: Int): [Cat]
+    breedCatImages(breedId: String!, limit: Int): [Cat]
   }
 `;
+
